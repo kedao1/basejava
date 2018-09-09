a385458d8c1dc8ebc482d4e2d5d63662be9f5434
kedao1 <kedao1@163.com>
2018-09-10 06:53:32 +0800

    注解讲解案例

--- src/cn/dl/jcdz/annotation/AnnoTest.java
+++ src/cn/dl/jcdz/annotation/AnnoTest.java
@@ -1,4 +1,14 @@
 package cn.dl.jcdz.annotation;
 
+import jdk.nashorn.internal.ir.annotations.Reference;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE.TYPE,ElementType.ANNOTATION_TYPE.FIELD,ElementType.ANNOTATION_TYPE.METHOD})
 public @interface AnnoTest {
+    String value() default "-1";
 }
---
+++ src/cn/dl/jcdz/annotation/Main.java
@@ -0,0 +1,9 @@
+package cn.dl.jcdz.annotation;
+
+public class Main {
+    public static void  main(String[] args){
+        Person person = new Person();
+        person.speak();
+        person.say();
+    }
+}
