2eff39f1fc7ce91834a4d50b34739b585218d304
kedao1 <kedao1@163.com>
2018-09-12 06:29:38 +0800

    operate和测试类完成

---
+++ src/cn/dl/jcdz/juinit/Operate.java
@@ -0,0 +1,20 @@
+package cn.dl.jcdz.juinit;
+
+public class Operate {
+
+	public int sum(int a, int b) {
+		return a + b;
+	}
+
+	public int sub(int a, int b) {
+		return a - b;
+	}
+
+	public int mult(int a, int b) {
+		return a * b;
+	}
+
+	public int div(int a, int b) {
+		return a / b;
+	}
+}
