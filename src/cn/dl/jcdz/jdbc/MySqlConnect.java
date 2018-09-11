d6a5179dc584527d86f174f107099f33f53665cb
kedao1 <kedao1@163.com>
2018-09-12 07:41:13 +0800

    jdbc

--- src/cn/dl/jcdz/jdbc/MySqlConnect.java
+++ src/cn/dl/jcdz/jdbc/MySqlConnect.java
@@ -7,13 +7,16 @@ import java.sql.Statement;
 
 public class MySqlConnect {
 	public static void main(String[] args) throws Exception {
-		String URL="jdbc:mysql://127.0.0.1:3306/test?useUnicode=true&amp;characterEncoding=utf-8";
+		String URL="jdbc:mysql://127.0.0.1:3306/test";
         String USER="root";
         String PASSWORD="root";
+        
         //1.加载mysql驱动程序
         Class.forName("com.mysql.jdbc.Driver");
+       
         //2.建立数据库链接
         Connection conn=DriverManager.getConnection(URL, USER, PASSWORD);
+        
         //3.连接操作数据库，实现增删改查
         Statement st=conn.createStatement();
         ResultSet rs=st.executeQuery("select * from user");
