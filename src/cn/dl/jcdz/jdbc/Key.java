3877ac43d14e03575090ea1571d741a97ce8a6f7
kedao1 <kedao1@163.com>
2018-09-17 07:32:30 +0800

    输入输出流综合案例讲解

---
+++ src/cn/dl/jcdz/jdbc/Key.java
@@ -0,0 +1,22 @@
+package cn.dl.jcdz.jdbc;
+
+public class Key {
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    private String title;
+    private String url;
+}
